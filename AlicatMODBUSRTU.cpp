6f875a6b8957e81de9a2974cbcb0f4e1653e245e
William Stoy <ws2574@columbia.edu>
2023-06-07 15:30:36 -0400

    Initial commit

---
+++ AlicatMODBUSRTU.cpp
@@ -0,0 +1,577 @@
+// reference: https://documents.alicat.com//manuals/DOC-MANUAL-MPL.pdf
+/*
+Sensor and process values are stored as big-endian, 32-bit
+IEEE-754 floating point numbers spanning two registers. Your
+control system will need to chain these into a single value to
+interpret them correctly.
+*/
+#include <Arduino.h>
+#include <ModbusInterface.h>
+#include <AlicatMODBUSRTU.h>
+
+
+
+AlicatMODBUSRTU::AlicatMODBUSRTU(int modbusID, int deviceType, ModbusInterface& modbus, HardwareSerial& serial, bool verbose) 
+: _modbusID(modbusID), _modbus(modbus), _serial(serial), _verbose(verbose), _deviceType(deviceType)
+{
+  _registerOffset       = -1; // default register offset. Apparently this is a MODBUS Standard
+  _connected            = false;
+}
+
+
+/** 
+ * HELPER FUNCTIONS
+*/
+void AlicatMODBUSRTU::setRegisterOffset(int registerOffset) {
+    _registerOffset = registerOffset;
+}
+
+
+
+void AlicatMODBUSRTU::setModbusID(int modbusID) {
+    _modbusID = modbusID;
+}
+
+
+
+int AlicatMODBUSRTU::offsetRegister(int address) {
+    return address + _registerOffset;
+}
+
+
+
+bool AlicatMODBUSRTU::isConnected() {
+  return _connected;
+}
+
+
+
+void AlicatMODBUSRTU::getDeviceStatisticRegisterAddress(int statisticIndex, int *registerAddress) {
+  if (statisticIndex < 1 || statisticIndex > 20) {
+    if (_verbose) _serial.println("ERROR: function:'getDeviceStatisticRegisterAddress', argument statisticIndex is out of bounds");
+
+    return;
+  }
+
+  *registerAddress = REGISTER_DEVICE_STATISTIC_1_VALUE + 2*(statisticIndex - 1);
+}
+
+
+
+/**
+ * READ AND WRITE REGISTER FUNCTIONS
+*/
+void AlicatMODBUSRTU::readSingleRegister(int registerAddress, uint16_t *registerValue) {
+  const int dataLength = 1;
+  uint16_t response[dataLength];
+
+  if (!_modbus.readHoldingRegisterValues(_modbusID, offsetRegister(registerAddress), dataLength, response)) {
+      _connected = false;
+
+      _serial.print("ERROR: Failed to read register: ");
+      _serial.println(registerAddress);
+      return;
+  }
+
+  _connected = true;
+
+  // concatenate the two response bytes into a single integer and return
+  *registerValue = response[0]; // @todo: get rid of this (response[1] << 8) + response[0];
+}
+
+
+
+void AlicatMODBUSRTU::readRegistersAsFloat(int registerAddress, float *floatValue) {
+  const int dataLength = 2;
+  uint16_t response[dataLength];
+
+  union {
+    float asFloat;
+    uint16_t asBytes[dataLength];
+  } floatValueUnion;
+
+  if (!_modbus.readHoldingRegisterValues(_modbusID, offsetRegister(registerAddress), dataLength, response)) {
+      _connected = false;
+
+      _serial.print("ERROR: Failed to read register: ");
+      _serial.println(registerAddress);
+      return;
+  }
+
+  _connected = true;
+
+  // concatenate the response bytes into a single float using the following format:
+  // All 32-bit values are handled in consecutive Modbus registers in big-
+  // endian format. This means bits 31:16 are in the lower numbered Modbus
+  // register and bits 15:0 are in the higher register. All floating-point values
+  // are IEEE 32-bit floats.
+
+  floatValueUnion.asBytes[1] = response[0];
+  floatValueUnion.asBytes[0] = response[1];
+
+  *floatValue = floatValueUnion.asFloat;
+}
+
+
+
+void AlicatMODBUSRTU::writeRegistersAsFloat(int registerAddress, float floatValue) {
+  const int dataLength = 2;
+  
+  union {
+    float asFloat;
+    uint16_t asBytes[dataLength];
+  } floatValueUnion;
+
+  floatValueUnion.asFloat = floatValue;
+
+  uint16_t data[dataLength];
+  for(int i = 0; i < dataLength; i++) {
+    data[i] = floatValueUnion.asBytes[dataLength - i - 1];
+  }
+
+  _modbus.writeHoldingRegisterValues(_modbusID, offsetRegister(registerAddress), data, dataLength);
+}
+
+
+
+void AlicatMODBUSRTU::writeSingleRegister(int registerAddress, uint16_t registerValue) {
+  const int dataLength = 1;
+  uint16_t registerValueArray[dataLength] = { registerValue };
+
+  _modbus.writeHoldingRegisterValues(_modbusID, offsetRegister(registerAddress), registerValueArray, dataLength);
+}
+
+
+/**
+ * MODBUS READING AND STATUS REGISTERS
+*/
+void AlicatMODBUSRTU::setSetpoint(float setpoint) {
+  if (!deviceIsController()) {
+    if (_verbose) _serial.println("ERROR: function, 'setSetpoint' is not used for devices of this type");
+
+    return;
+  }
+
+  writeRegistersAsFloat(REGISTER_SETPOINT, setpoint);
+}
+
+
+
+void AlicatMODBUSRTU::getSetpoint(float *setPoint) {
+  if (!deviceIsController()) {
+    if (_verbose) _serial.println("ERROR: function, 'setSetpoint' is not used for devices of this type");
+
+    return;
+  }
+
+  int registerAddress;
+
+  if (deviceIsMassFlow()) {
+    getDeviceStatisticRegisterAddress(5, &registerAddress);
+  } else if (deviceIsPressureController()) {
+    getDeviceStatisticRegisterAddress(2, &registerAddress);
+  }
+
+  readRegistersAsFloat(registerAddress, setPoint);
+}
+
+
+
+void AlicatMODBUSRTU::getPressure(float *pressure) {
+  // all devices have a pressure statistic
+
+  int registerAddress;
+  
+  getDeviceStatisticRegisterAddress(1, &registerAddress);
+
+  readRegistersAsFloat(registerAddress, pressure);
+}
+
+
+
+void AlicatMODBUSRTU::setMixtureGasProperties(int mixtureIndex, uint16_t gasIndex, float gasPercent) {
+  if (!deviceIsMassFlow()) {
+    if (_verbose) _serial.println("ERROR: function, 'setMixtureGasProperties' is not used for devices of this type");
+
+    return;
+  }
+
+  // check to make sure the mixture index is between 1 and 5
+  if (mixtureIndex < 1 || mixtureIndex > 5) {
+    if (_verbose) _serial.println("ERROR: function, 'setMixtureGasProperties', mixtureIndex must be between 1 and 5");
+
+    return;
+  }
+
+  // check to make sure the gas index is between 0 and 210
+  if (gasIndex < 0 || gasIndex > 210) {
+    if (_verbose) _serial.println("ERROR: function, 'setMixtureGasProperties', gasIndex must be between 0 and 210");
+
+    return;
+  }
+
+  // check to make sure the gas percent is between 0 and 100
+  if (gasPercent < 0.0 || gasPercent > 100.0) {
+    if (_verbose) _serial.println("ERROR: function, 'setMixtureGasProperties', gasPercent must be between 0 and 100");
+
+    return;
+  }
+
+  int gasIndexRegisterAddress    = REGISTER_MIXTURE_GAS_1_INDEX + 2*(mixtureIndex - 1);
+  int gasPercentRegisterAddress  = gasIndexRegisterAddress + 1;
+
+  // write the gas index
+  writeSingleRegister(gasIndexRegisterAddress, gasIndex);
+
+  // write the gas percent
+  // "...to specify a mix of 50%, a value of 5000 is written into the gas percentage register."
+  uint16_t gasPercentInt = (uint16_t)round(gasPercent * 100.0);
+
+  writeSingleRegister(gasPercentRegisterAddress, gasPercentInt);
+}
+
+
+
+void AlicatMODBUSRTU::getMixtureGasProperties(int mixtureIndex, uint16_t *gasIndex, float *gasPercent) {
+  if (!deviceIsMassFlow()) {
+    if (_verbose) _serial.println("ERROR: function, 'getMixtureGasProperties' is not used for devices of this type");
+
+    return;
+  }
+
+  // check to make sure the mixture index is between 1 and 5
+  if (mixtureIndex < 1 || mixtureIndex > 5) {
+    if (_verbose) _serial.println("ERROR: function, 'getMixtureGasProperties', mixtureIndex must be between 1 and 5");
+
+    return;
+  }
+
+  int gasIndexRegisterAddress    = REGISTER_MIXTURE_GAS_1_INDEX + (mixtureIndex - 1);
+  int gasPercentRegisterAddress  = gasIndexRegisterAddress + 1;
+
+  // read the gas index
+  readSingleRegister(gasIndexRegisterAddress, gasIndex);
+
+  // read the gas percent
+  uint16_t gasPercentageInt;
+  readSingleRegister(gasPercentRegisterAddress, &gasPercentageInt);
+
+  *gasPercent = ((float)gasPercentageInt) / 100.0;
+}
+
+
+
+void AlicatMODBUSRTU::setGasNumber(uint16_t gasIndex) {
+  if (!deviceIsMassFlow()) {
+    if (_verbose) _serial.println("ERROR: function, 'setGasNumber' is not used for devices of this type");
+
+    return;
+  }
+
+  // check to make sure the gas index is between 0 and 210
+  if (gasIndex < 0 || gasIndex > 210) {
+    if (_verbose) _serial.println("ERROR: function, 'setGasNumber', gasIndex must be between 0 and 210");
+
+    return;
+  }
+
+  writeSingleRegister(REGISTER_GAS_NUMBER, gasIndex);
+}
+
+
+
+void AlicatMODBUSRTU::getGasNumber(uint16_t *gasIndex) {
+  if (!deviceIsMassFlow()) {
+    if (_verbose) _serial.println("ERROR: function, 'getGasNumber' is not used for devices of this type");
+
+    return;
+  }
+
+  readSingleRegister(REGISTER_GAS_NUMBER, gasIndex);
+}
+
+
+
+void AlicatMODBUSRTU::getStatusFlags() {
+    uint16_t status;
+    readSingleRegister(REGISTER_DEVICE_STATUS, &status);
+
+    _status.TEMPERATURE_OVERFLOW          = status & STATUS_BIT_TEMPERATURE_OVERFLOW;
+    _status.TEMPERATURE_UNDERFLOW         = status & STATUS_BIT_TEMPERATURE_UNDERFLOW;
+    _status.VOLUMETRIC_OVERFLOW           = status & STATUS_BIT_VOLUMETRIC_OVERFLOW;
+    _status.VOLUMETRIC_UNDERFLOW          = status & STATUS_BIT_VOLUMETRIC_UNDERFLOW;
+    _status.MASS_OVERFLOW                 = status & STATUS_BIT_MASS_OVERFLOW;
+    _status.MASS_UNDERFLOW                = status & STATUS_BIT_MASS_UNDERFLOW;
+    _status.PRESSURE_OVERFLOW             = status & STATUS_BIT_PRESSURE_OVERFLOW;
+    _status.TOTALIZER_OVERFLOW            = status & STATUS_BIT_TOTALIZER_OVERFLOW;
+    _status.PID_LOOP_IN_HOLD              = status & STATUS_BIT_PID_LOOP_IN_HOLD;
+    _status.ADC_ERROR                     = status & STATUS_BIT_ADC_ERROR;
+    _status.PID_EXHAUST                   = status & STATUS_BIT_PID_EXHAUST;
+    _status.OVER_PRESSURE_LIMIT           = status & STATUS_BIT_OVER_PRESSURE_LIMIT;
+    _status.FLOW_OVERFLOW_DURING_TOTALIZE = status & STATUS_BIT_FLOW_OVERFLOW_DURING_TOTALIZE;
+    _status.MEASUREMENT_ABORTED           = status & STATUS_BIT_MEASUREMENT_ABORTED;
+    _status.ANY_ERROR                     = status > 0;
+
+    if (_verbose) {
+        _serial.print("STATUS Bits: ");
+        _serial.println(status, BIN);
+
+        if (_status.TEMPERATURE_OVERFLOW)           _serial.println("STATUS: TEMPERATURE OVERFLOW bit is set");
+        if (_status.TEMPERATURE_UNDERFLOW)          _serial.println("STATUS: TEMPERATURE UNDERFLOW bit is set");
+        if (_status.VOLUMETRIC_OVERFLOW)            _serial.println("STATUS: VOLUMETRIC OVERFLOW bit is set");
+        if (_status.VOLUMETRIC_UNDERFLOW)           _serial.println("STATUS: VOLUMETRIC UNDERFLOW bit is set");
+        if (_status.MASS_OVERFLOW)                  _serial.println("STATUS: MASS OVERFLOW bit is set");
+        if (_status.MASS_UNDERFLOW)                 _serial.println("STATUS: MASS UNDERFLOW bit is set");
+        if (_status.PRESSURE_OVERFLOW)              _serial.println("STATUS: PRESSURE OVERFLOW bit is set");
+        if (_status.TOTALIZER_OVERFLOW)             _serial.println("STATUS: TOTALIZER OVERFLOW bit is set");
+        if (_status.PID_LOOP_IN_HOLD)               _serial.println("STATUS: PID LOOP IN HOLD bit is set");
+        if (_status.ADC_ERROR)                      _serial.println("STATUS: ADC ERROR bit is set");
+        if (_status.PID_EXHAUST)                    _serial.println("STATUS: PID EXHAUST bit is set");
+        if (_status.OVER_PRESSURE_LIMIT)            _serial.println("STATUS: OVER PRESSURE LIMIT bit is set");
+        if (_status.FLOW_OVERFLOW_DURING_TOTALIZE)  _serial.println("STATUS: FLOW OVERFLOW DURING TOTALIZE bit is set");
+        if (_status.MEASUREMENT_ABORTED)            _serial.println("STATUS: MEASUREMENT ABORTED bit is set");
+    }
+}
+
+
+
+void AlicatMODBUSRTU::getFlowTemperature(float *flowTemperature) {
+  if (!deviceIsMassFlow() && !deviceIsLiquid()) {
+    if (_verbose) _serial.println("ERROR: function, 'getFlowTemperature' is not used for devices of this type");
+
+    return;
+  }
+
+  int registerAddress;
+  
+  getDeviceStatisticRegisterAddress(2, &registerAddress);
+
+  readRegistersAsFloat(registerAddress, flowTemperature);
+}
+
+
+
+void AlicatMODBUSRTU::getVolumetricFlow(float *volumetricFlow) {
+  if (!deviceIsMassFlow() && !deviceIsLiquid()) {
+    if (_verbose) _serial.println("ERROR: function, 'getVolumetricFlow' is not used for devices of this type");
+
+    return;
+  }
+
+  int registerAddress;
+  
+  getDeviceStatisticRegisterAddress(3, &registerAddress);
+
+  readRegistersAsFloat(registerAddress, volumetricFlow);
+}
+
+
+
+void AlicatMODBUSRTU::getMassFlow(float *massFlow) {
+  if (!deviceIsMassFlow()) {
+    if (_verbose) _serial.println("ERROR: function, 'getMassFlow' is not used for devices of this type");
+
+    return;
+  }
+
+  int registerAddress;
+  
+  getDeviceStatisticRegisterAddress(4, &registerAddress);
+
+  readRegistersAsFloat(registerAddress, massFlow);
+}
+
+
+
+void AlicatMODBUSRTU::getMassTotal(float *massTotal) {
+  if (!deviceIsMassFlow()) {
+    if (_verbose) _serial.println("ERROR: function, 'getMassFlow' is not used for devices of this type");
+
+    return;
+  }
+
+  int registerAddress;
+
+  if (deviceIsController()) {
+    getDeviceStatisticRegisterAddress(6, &registerAddress);
+  } else {
+    getDeviceStatisticRegisterAddress(5, &registerAddress);
+  }
+
+  readRegistersAsFloat(registerAddress, massTotal);
+}
+
+
+
+/**
+ * SPECIAL COMMANDS
+*/
+bool AlicatMODBUSRTU::sendSpecialCommand(uint16_t command, uint16_t argument) {
+  uint16_t data[2] = { command, argument };
+
+  _modbus.writeHoldingRegisterValues(_modbusID, offsetRegister(REGISTER_COMMAND_ID), data, 2);
+
+  uint16_t status;
+  readSingleRegister(REGISTER_COMMAND_ARGUMENT, &status);
+
+  return handleSpecialCommandStatusCode(status);
+}
+
+
+
+bool AlicatMODBUSRTU::handleSpecialCommandStatusCode(uint16_t status) {  
+  switch(status) {
+    case STATUS_CODE_SUCCESS:
+      //_serial.println("SUCCESSFUL STATUS CODE RETURNED");
+      return true;
+      break;
+    case STATUS_CODE_INVALID_COMMAND_ID:
+      if (_verbose) _serial.println("ERROR: INVALID COMMAND ID");
+      break;
+    case STATUS_CODE_INVALID_SETTING:
+      if (_verbose) _serial.println("ERROR: INVALID SETTING");
+      break;
+    case STATUS_CODE_REQUESTED_FEATURE_IS_UNSUPPORTED:
+      if (_verbose)  _serial.println("ERROR: REQUESTED FEATURE IS UNSUPPORTED");
+      break;
+    case STATUS_CODE_INVALID_GAS_MIX_INDEX:
+      if (_verbose) _serial.println("ERROR: INVALID GAS MIX INDEX (MASS FLOW DEVICE)");
+      break;
+    case STATUS_CODE_INVALID_GAS_MIX_CONSTITUENT:
+      if (_verbose) _serial.println("ERROR: INVALID GAS MIX CONSTITUENT (MASS FLOW DEVICE)");
+      break;
+    case STATUS_CODE_INVALID_GAS_MIX_PERCENTAGE:
+      if (_verbose) _serial.println("ERROR: INVALID GAS MIX PERCENTAGE (MASS FLOW DEVICE)");
+      break;
+    default:
+      if (_verbose) _serial.println("ERROR: UNKNOWN STATUS CODE");
+      break;
+  }
+
+  return false;
+}
+
+
+
+void AlicatMODBUSRTU::readPIDValue(uint16_t coefficientID, uint16_t *coefficientValue) {
+  if (!deviceIsController()) {
+    if (_verbose) _serial.println("ERROR: function, 'readPIDValue' is not used for devices of this type");
+
+    return;
+  }
+
+  if (coefficientID < 0 || coefficientID > 2) {
+    if (_verbose) _serial.println("ERROR: function:'readPIDValue', argument coefficientID is out of bounds");
+
+    return;
+  }
+
+  sendSpecialCommand(SPECIAL_COMMAND_READ_PID_VALUE, coefficientID);
+
+  readSingleRegister(REGISTER_COMMAND_ARGUMENT, coefficientValue);
+}
+
+
+
+void AlicatMODBUSRTU::changeGasNumber(uint16_t gasTableIndex) {
+  if (!deviceIsMassFlow()) {
+    if (_verbose) _serial.println("ERROR: function, 'changeGasNumber' is not used for devices of this type");
+
+    return;
+  }
+
+  sendSpecialCommand(SPECIAL_COMMAND_CHANGE_GAS_NUMBER, gasTableIndex);
+}
+
+
+
+void AlicatMODBUSRTU::createCustomGasMixture(uint16_t gasMixtureIndex) {
+  if (!deviceIsMassFlow()) {
+    if (_verbose) _serial.println("ERROR: function, 'createCustomGasMixture' is not used for devices of this type");
+    
+    return;
+  }
+
+  if (gasMixtureIndex != 0 && (gasMixtureIndex < 236 || gasMixtureIndex > 255)) {
+    if (_verbose) _serial.println("ERROR: function:'createCustomGasMixture', argument gasMixtureIndex is out of bounds");
+
+    return;
+  }
+
+  sendSpecialCommand(SPECIAL_COMMAND_CREATE_CUSTOM_GAS_MIXTURE, gasMixtureIndex);
+}
+
+
+
+void AlicatMODBUSRTU::deleteCustomGasMixture(uint16_t gasMixtureIndex) {
+  if (!deviceIsMassFlow()) {
+    if (_verbose) _serial.println("ERROR: function, 'deleteCustomGasMixture' is not used for devices of this type");
+    
+    return;
+  }
+
+  sendSpecialCommand(SPECIAL_COMMAND_DELETE_CUSTOM_GAS_MIXTURE, gasMixtureIndex);
+}
+
+
+
+void AlicatMODBUSRTU::tare(uint16_t tareArgument) {
+  bool proceed = true;
+
+  switch(tareArgument) {
+    case 0:
+      if(!deviceIsPressureController()) proceed = false;
+      break;
+    case 1:
+      if(!deviceIsPressureController()) proceed = false;
+      break;
+    case 2:
+      if(!deviceIsMassFlow() && !deviceIsLiquid()) proceed = false;
+      break;
+    default:
+      proceed = false;
+      break;
+  }
+
+  if (!proceed) {
+    if (_verbose) {
+      _serial.print("ERROR: Incorrect Tare Argument ");
+      _serial.print(tareArgument);
+      _serial.print(" for device of type");
+      _serial.println(_deviceType);
+    }
+
+    return;
+  }
+
+  sendSpecialCommand(SPECIAL_COMMAND_TARE, tareArgument);
+}
+
+
+
+/**
+ * DEVICE TYPE CHECK
+*/
+bool AlicatMODBUSRTU::deviceIsMassFlow() {
+  return _deviceType == DEVICE_TYPE_MASS_FLOW_METER || _deviceType == DEVICE_TYPE_MASS_FLOW_CONTROLLER;
+}
+
+
+
+bool AlicatMODBUSRTU::deviceIsController() {
+  return _deviceType == DEVICE_TYPE_PSID_CONTROLLER || _deviceType == DEVICE_TYPE_GAUGE_PRESSURE_CONTROLLER || _deviceType == DEVICE_TYPE_MASS_FLOW_CONTROLLER;
+}
+
+
+
+bool AlicatMODBUSRTU::deviceIsPressureController() {
+  return _deviceType == DEVICE_TYPE_PSID_CONTROLLER || _deviceType == DEVICE_TYPE_GAUGE_PRESSURE_CONTROLLER;
+}
+
+
+
+bool AlicatMODBUSRTU::deviceIsLiquid() {
+  return _deviceType == DEVICE_TYPE_LIQUID_CONTROLLER;
+}
+
+
---
+++ AlicatMODBUSRTU.h
@@ -0,0 +1,128 @@
+// reference: https://documents.alicat.com//manuals/DOC-MANUAL-MPL.pdf
+// also reference: https://documents.alicat.com/manuals/ModbusRTU_Manual.pdf
+
+#ifndef AlicatMODBUSRTU_h
+    #define AlicatMODBUSRTU_h
+    #include <Arduino.h>
+    #include <ModbusInterface.h>
+
+    #define DEVICE_TYPE_MASS_FLOW_CONTROLLER                0
+    #define DEVICE_TYPE_LIQUID_CONTROLLER                   1
+    #define DEVICE_TYPE_MASS_FLOW_METER                     2
+    #define DEVICE_TYPE_PSID_CONTROLLER                     3
+    #define DEVICE_TYPE_GAUGE_PRESSURE_CONTROLLER           4                               
+
+    // @todo: not sure if I want to use an enum here
+    #define STATUS_CODE_SUCCESS                             0       // All
+    #define STATUS_CODE_INVALID_COMMAND_ID                  32769   // All
+    #define STATUS_CODE_INVALID_SETTING                     32770   // All
+    #define STATUS_CODE_REQUESTED_FEATURE_IS_UNSUPPORTED    32771   // All
+    #define STATUS_CODE_INVALID_GAS_MIX_INDEX               32772   // Mass Flow
+    #define STATUS_CODE_INVALID_GAS_MIX_CONSTITUENT         32773   // Mass Flow
+    #define STATUS_CODE_INVALID_GAS_MIX_PERCENTAGE          32774   // Mass Flow
+
+    #define STATUS_BIT_TEMPERATURE_OVERFLOW                 0x0001  // Interpretation: Temperature Overflow (TOV), Devices: Mass Flow and Liquid
+    #define STATUS_BIT_TEMPERATURE_UNDERFLOW                0x0002  // Interpretation: Temperature Underflow (TOV), Devices: Mass Flow and Liquid
+    #define STATUS_BIT_VOLUMETRIC_OVERFLOW                  0x0004  // Interpretation: Volumetric Overflow (VOV), Devices: Mass Flow and Liquid
+    #define STATUS_BIT_VOLUMETRIC_UNDERFLOW                 0x0008  // Interpretation: Volumetric Underflow (VOV), Devices: Mass Flow
+    #define STATUS_BIT_MASS_OVERFLOW                        0x0010  // Interpretation: Mass Overflow (MOV), Devices: Mass Flow
+    #define STATUS_BIT_MASS_UNDERFLOW                       0x0020  // Interpretation: Mass Underflow (MOV), Devices: Mass Flow
+    #define STATUS_BIT_PRESSURE_OVERFLOW                    0x0040  // Interpretation: Pressure Overflow (POV), Devices: All
+    #define STATUS_BIT_TOTALIZER_OVERFLOW                   0x0080  // Interpretation: Totalizer Overflow (OVR), Devices: Mass Flow and Liquid
+    #define STATUS_BIT_PID_LOOP_IN_HOLD                     0x0100  // Interpretation: PID Loop in Hold (HLD), Devices: Controllers
+    #define STATUS_BIT_ADC_ERROR                            0x0200  // Interpretation: ADC Error (ADC), Devices: All
+    #define STATUS_BIT_PID_EXHAUST                          0x0400  // Interpretation: PID Exhaust (EXH), Devices: Dual Valve Controllers
+    #define STATUS_BIT_OVER_PRESSURE_LIMIT                  0x0800  // Interpretation: Over pressure limit (OPL), Devices: Custom OPL devices
+    #define STATUS_BIT_FLOW_OVERFLOW_DURING_TOTALIZE        0x1000  // Interpretation: Flow overflow during totalize (TMF), Devices: Mass Flow and Liquid
+    #define STATUS_BIT_MEASUREMENT_ABORTED                  0x2000  // Interpretation: Measurement was aborted, Devices: All
+
+    #define SPECIAL_COMMAND_CHANGE_GAS_NUMBER               1       // Mass Flow
+    #define SPECIAL_COMMAND_CREATE_CUSTOM_GAS_MIXTURE       2       // Mass Flow
+    #define SPECIAL_COMMAND_DELETE_CUSTOM_GAS_MIXTURE       3       // Mass Flow
+    #define SPECIAL_COMMAND_TARE                            4       // All
+    #define SPECIAL_COMMAND_RESET_TOTALIZER_VALUE           5       // Mass Flow and Liquid
+    #define SPECIAL_COMMAND_VALVE_SETTING                   6       // Controllers
+    #define SPECIAL_COMMAND_DISPLAY_LOCK                    7       // All
+    #define SPECIAL_COMMAND_CHANGE_P_IN_PID_LOOP            8       // Controllers
+    #define SPECIAL_COMMAND_CHANGE_D_IN_PID_LOOP            9       // Controllers
+    #define SPECIAL_COMMAND_CHANGE_I_IN_PID_LOOP            10      // Controllers
+    #define SPECIAL_COMMAND_CHANGE_CONTROL_LOOP_VARIABLE    11      // All
+    #define SPECIAL_COMMAND_SAVE_CURRENT_SETPOINT_TO_MEMORY 12      // Controllers
+    #define SPECIAL_COMMAND_CHANGE_LOOP_CONTROL_ALGORITHM   13      // Controllers
+    #define SPECIAL_COMMAND_READ_PID_VALUE                  14      // Controllers
+    #define SPECIAL_COMMAND_CHANGE_MODBUS_ID                32767   // All
+
+    #define REGISTER_COMMAND_ID                             1000    // Access: Read/Write,  Devices: All
+    #define REGISTER_COMMAND_ARGUMENT                       1001    // Access: Read/Write,  Devices: All
+    #define REGISTER_SETPOINT                               1010    // Access: Write,       Devices: Controllers (MPL Manual says this is R/W, but it appears to be W only)
+    #define REGISTER_MIXTURE_GAS_1_INDEX                    1050    // Access: Read/Write,  Devices: Mass Flow (All gas mixture indicies, n, can be accessed by adding 2*(n-1) to this starting register value)
+    #define REGISTER_MIXTURE_GAS_1_PERCENT                  1051    // Access: Read/Write,  Devices: Mass Flow (All gas mixture percents, n, can be accessed by adding 2*(n-1)+1 to the starting register value of REGISTER_MIXTURE_GAS_1_INDEX)
+    #define REGISTER_GAS_NUMBER                             1200    // Access: Read/Write,  Devices: Mass Flow
+    #define REGISTER_DEVICE_STATUS                          1201    // Access: Read,        Devices: All
+    #define REGISTER_DEVICE_STATISTIC_1_VALUE               1203    // Access: Read,        Devices: All (All device statistic values, n, can be accessed by adding 2*(n-1) to this starting register value)
+    #define REGISTER_MASS_FLOW                              1209    // Access: Read,        Devices: Mass Flow
+
+    class AlicatMODBUSRTU {
+        private:
+            HardwareSerial&     _serial;                            // Use a reference here, not a value
+            ModbusInterface&    _modbus;                            // Use a reference here, not a value
+            bool                _connected;
+            bool                _verbose;
+            int                 _registerOffset;
+            int                 _modbusID;
+            int                 _deviceType;
+
+            struct {
+                bool            TEMPERATURE_OVERFLOW;
+                bool            TEMPERATURE_UNDERFLOW;
+                bool            VOLUMETRIC_OVERFLOW;
+                bool            VOLUMETRIC_UNDERFLOW;
+                bool            MASS_OVERFLOW;
+                bool            MASS_UNDERFLOW;
+                bool            PRESSURE_OVERFLOW;
+                bool            TOTALIZER_OVERFLOW;
+                bool            PID_LOOP_IN_HOLD;
+                bool            ADC_ERROR;
+                bool            PID_EXHAUST;
+                bool            OVER_PRESSURE_LIMIT;
+                bool            FLOW_OVERFLOW_DURING_TOTALIZE;
+                bool            MEASUREMENT_ABORTED;
+                bool            ANY_ERROR;
+            } _status;
+
+        public:
+                 AlicatMODBUSRTU(int modbusID, int deviceType, ModbusInterface& modbus, HardwareSerial& serial, bool verbose);
+            void setRegisterOffset(int registerOffset);
+            void setModbusID(int modbusID);
+            int  offsetRegister(int address);
+            void getGasNumber(uint16_t *gasIndex);
+            void getStatusFlags();
+            void getFlowTemperature(float *flowTemperature);
+            void getVolumetricFlow(float *volumetricFlow);
+            void getMassFlow(float *massFlow);
+            void getMassTotal(float *massTotal);
+            bool sendSpecialCommand(uint16_t command, uint16_t argument);
+            bool handleSpecialCommandStatusCode(uint16_t statusCode);
+            void readPIDValue(uint16_t coefficientID, uint16_t *coefficientValue);
+            void changeGasNumber(uint16_t gasTableIndex);
+            void createCustomGasMixture(uint16_t gasMixtureIndex);
+            void deleteCustomGasMixture(uint16_t gasMixtureIndex);
+            bool isConnected();
+            void getDeviceStatisticRegisterAddress(int statisticIndex, int *registerAddress);
+            void readSingleRegister(int registerAddress, uint16_t *registerValue);
+            void readRegistersAsFloat(int registerAddress, float *floatValue);
+            void writeRegistersAsFloat(int registerAddress, float floatValue);
+            void writeSingleRegister(int registerAddress, uint16_t registerValue);
+            void setSetpoint(float setpoint);
+            void getSetpoint(float *setPoint);
+            void getPressure(float *pressure);
+            void setMixtureGasProperties(int mixtureIndex, uint16_t gasIndex, float gasPercent);
+            void getMixtureGasProperties(int mixtureIndex, uint16_t *gasIndex, float *gasPercent);
+            void setGasNumber(uint16_t gasIndex);
+            void tare(uint16_t tareArgument);
+            bool deviceIsMassFlow();
+            bool deviceIsController();
+            bool deviceIsPressureController();
+            bool deviceIsLiquid();
+    };
+#endif
